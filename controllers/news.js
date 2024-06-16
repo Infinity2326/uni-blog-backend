0dd728e37e40eec89cb49e3dec87cb271a6bea72
Infinity2326 <dima.gorbachev2002@mail.ru>
2024-06-16 22:12:02 +0300

    fix: remove request to news

--- controllers/news.js
+++ controllers/news.js
@@ -5,52 +5,12 @@ import Comment from "../models/Comment.js"
 // Get news and save them in db
 export const getNews = async (req, res) => {
   try {
-    const url = "https://www.muiv.ru/about/novosti/"
-    async function start() {
-      const browser = await launch({
-        headless: false,
-        defaultViewport: null,
-      })
-
-      const page = await browser.newPage()
-      await page.goto(url, { waitUntil: "domcontentloaded" })
-
-      const result = await page.evaluate(() => {
-        const news = { news: [] }
-        document.querySelectorAll(".row").forEach(async (n) => {
-          const imgElement = n.querySelector(".col-lg-5 div div")
-          const title = n.querySelector(".col-lg-7 h3")?.innerHTML
-          const text = n.querySelector(".col-lg-7 p")?.innerHTML
-          const imgUrl = `https://muiv.ru${imgElement
-            ?.getAttribute("style")
-            .slice(23, 88)}`
-          news.news.push({
-            title,
-            text,
-            imgUrl,
-          })
-        })
-        return news
-      })
-
-      result.news.forEach(async (n) => {
-        if (!(await News.findOne({ imgUrl: n.imgUrl }).exec())) {
-          const singleNews = new News({
-            title: n.title,
-            text: n.text,
-            imgUrl: n.imgUrl,
-          })
-          await singleNews.save()
-        }
-      })
-      const news = await News.find()
-      if (!news) {
-        return res.json({ message: "Новостей не существует" })
-      }
-      res.json({ news })
-      await browser.close()
+    const news = await News.find()
+    if (!news) {
+      return res.json({ message: "Новостей нет" })
     }
-    start()
+
+    res.json({ news })
   } catch (error) {
     res.json({ message: "Что-то пошло не так." })
   }
